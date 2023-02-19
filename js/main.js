924b2a8d592d0f319ca35380f007eda05bb3db74
Vitalik0422 <vitaliy19980422@gmail.com>
2023-02-19 19:25:00 +0200

    19.02.2023

--- js/main.js
+++ js/main.js
@@ -6,21 +6,23 @@ const gender = document.querySelector('.gender')
 
 const arr = []
 const genderPicUrl = [
-    {gender: 1, url: 'https://cdn-icons-png.flaticon.com/512/74/74540.png'}
+    {gender: 1, url: 'https://cdn-icons-png.flaticon.com/512/74/74540.png'},
+    {gender: 2, url: 'https://e7.pngegg.com/pngimages/278/881/png-clipart-gender-symbol-female-symbol-miscellaneous-cdr-thumbnail.png'},
+    {gender: 3, url: 'https://static.thenounproject.com/png/1629805-200.png'}
 ]
 submitBtn.addEventListener('click', () => {
     if (inp.value == ''){
         return
     } else if( gender.value == 1){
-        arr.push( {name: inp.value, male: 1, url: 'https://cdn-icons-png.flaticon.com/512/74/74540.png'} ); 
+        arr.push( {name: inp.value, url: genderPicUrl[0].url }); 
         render(gender.value)
         return
     } else if( gender.value == 2){
-        arr.push( {name: inp.value, male: 2, url: 'https://e7.pngegg.com/pngimages/278/881/png-clipart-gender-symbol-female-symbol-miscellaneous-cdr-thumbnail.png'} ); 
+        arr.push( {name: inp.value, url: genderPicUrl[1].url }); 
         render(gender.value)
         return
     } else {
-        arr.push( {name: inp.value, male: 3, url: 'https://static.thenounproject.com/png/1629805-200.png'} ); 
+        arr.push( {name: inp.value, url: genderPicUrl[2].url }); 
         render(gender.value)
         return
     } 
@@ -32,7 +34,6 @@ clearBtn.addEventListener('click', () => {
 })
 
 const render = () => {
-
     const html = arr.map((item)  => {
         html1 = `<div class="tabl">${item.name}</div><img src="${item.url}">`
         return html1;
